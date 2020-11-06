29fe49789540aa337b4efb226a6768b73ba3855b
nttzx <aghaspamntz@gmail.com>
2020-11-06 14:23:03 -0500

    Fixed RoomUserTagsEvent. (Thanks to Pollak!)

--- src/main/java/com/eu/habbo/messages/incoming/rooms/users/RequestRoomUserTagsEvent.java
+++ src/main/java/com/eu/habbo/messages/incoming/rooms/users/RequestRoomUserTagsEvent.java
@@ -1,20 +1,20 @@
 package com.eu.habbo.messages.incoming.rooms.users;
 
+import com.eu.habbo.Emulator;
 import com.eu.habbo.habbohotel.users.Habbo;
 import com.eu.habbo.messages.incoming.MessageHandler;
 import com.eu.habbo.messages.outgoing.rooms.users.RoomUserTagsComposer;
 
 public class RequestRoomUserTagsEvent extends MessageHandler {
+
     @Override
     public void handle() throws Exception {
-        int roomUnitId = this.packet.readInt();
+        int habboId = this.packet.readInt();
 
         if (this.client.getHabbo().getHabboInfo().getCurrentRoom() != null) {
-            Habbo habbo = this.client.getHabbo().getHabboInfo().getCurrentRoom().getHabboByRoomUnitId(roomUnitId);
-
-            if (habbo != null) {
+            Habbo habbo = Emulator.getGameEnvironment().getHabboManager().getHabbo(habboId);
                 this.client.sendResponse(new RoomUserTagsComposer(habbo));
-            }
         }
+
     }
 }
